41fbfef794c4244864bce137446629388693db47
Mika Hensel <41554385+MalTeeez@users.noreply.github.com>
2025-12-03 21:47:58 +0100

    Apply spotless

--- src/main/java/net/sxmaa/u25core/mixin/early/crashcatchers/ItemRendererMixin.java
+++ src/main/java/net/sxmaa/u25core/mixin/early/crashcatchers/ItemRendererMixin.java
@@ -1,10 +1,12 @@
 package net.sxmaa.u25core.mixin.early.crashcatchers;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.util.IIcon;
+
 import org.spongepowered.asm.mixin.Mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 
 @Mixin(value = ItemRenderer.class)
 public class ItemRendererMixin {
@@ -13,7 +15,6 @@ public class ItemRendererMixin {
     public void catchRenderInsideOfBlockCrash(float p_78446_1_, IIcon p_78446_2_, Operation<Void> original) {
         try {
             original.call(p_78446_1_, p_78446_2_);
-        } catch (NullPointerException ignored) {
-        }
+        } catch (NullPointerException ignored) {}
     }
 }
